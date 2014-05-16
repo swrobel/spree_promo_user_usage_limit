039b0a394446d4349acfb695885c031b4a2d33c7
Stefan Wrobel <swrobel@server.fake>
2014-05-16 08:37:43 -0700

    Completed orders only

--- app/models/spree/promotion/rules/user_usage_limit.rb
+++ app/models/spree/promotion/rules/user_usage_limit.rb
@@ -7,7 +7,7 @@ module Spree
 
         def eligible?(order, options = {})
           action_ids = promotion.actions.select(:id).pluck(:id)
-          promotion_count = Spree::Adjustment.promotion.where(adjustable_id: Spree::Order.select(:id).where(user_id: order.user.id).pluck(:id)).where(originator_id: action_ids).count
+          promotion_count = Spree::Adjustment.promotion.where(adjustable_id: Spree::Order.complete.select(:id).where(user_id: order.user.id).pluck(:id)).where(originator_id: action_ids).count
 
           promotion_count < preferred_uses
         end
